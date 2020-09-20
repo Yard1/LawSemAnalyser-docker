e298f3cf17366ae1330c8b8d7673fef3efd32471
Antoni Baum <antoni.baum@protonmail.com>
2020-09-20 22:44:09 +0200

    Add files via upload

---
+++ liner2-batch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+find /liner2/input -name *.txt | parallel --verbose "wcrft-app nkjp_e2.ini -i text -o ccl {} -O {}.tag"
+
+find /liner2/input -name *.tag | parallel --verbose "./liner2-cli pipe -i ccl -o json -f {} -m liner25_model_ner_rev1/config-top9.ini > /liner2/output/{/}.top9.json"
+
+find /liner2/input -name *.tag | parallel --verbose "./liner2-cli pipe -i ccl -o json -f {} -m timex_model_full/timex_model_full/cfg.ini > /liner2/output/{/}.timex.json"
